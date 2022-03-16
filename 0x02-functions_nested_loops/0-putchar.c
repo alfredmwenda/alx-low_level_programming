65e8a282d00810b9fdac2e6f3178318712654876
root <alfredmwenda>
2022-03-16 01:25:14 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,13 @@
 #include "myheader.h"
 
 /**
- * main - Print out reverse alphabet.
- *
- * Return: Always 0 (Success)
- */
+* main - Print out reverse alphabet.
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-<<<<<<< HEAD
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return (0);
-=======
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -30,5 +18,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
->>>>>>> 31eb3836a32cadf1681d59c8ba4e6f8bf29970ca
 }
