4f66996333c07945873793cef9e095138e75e131
root <alfredmwenda>
2022-03-17 14:22:23 -0700

    .

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - check if uppercase.
- * @c: ascii int value of the character to check
- *
- * Return: 1 if true else 0
- */
+* _isupper - check if uppercase.
+* @c: ascii int value of the character to check
+*
+* Return: 1 if true else 0
+*/
 
 int _isupper(int c)
 {
